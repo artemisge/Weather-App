1d23ffe1e7f00a394ec56cbcd7b81c454564e941
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2020-11-15 23:04:28 +0200

    Finished

--- main.js
+++ main.js
@@ -23,4 +23,36 @@ function getResults (query) {
 
 function displayResults(weather) {
     console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let minmax = document.querySelector('.current .hi-low');
+    minmax.innerHTML = `${Math.round(weather.main.temp_min)}<span>°C</span> / ${Math.round(weather.main.temp_max)}<span>°C</span>`
+
+    let looks = document.querySelector('.current .weather');
+    looks.innerHTML = `${weather.weather[0].main}`;
+}
+
+// makes and return whole date representation
+function dateBuilder(d) {
+
+    // uses months and days because getDay(), getMonth() returns integer 
+    let months = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    // return string of the date
+    return `${day} ${date} ${month} ${year}`
 }
